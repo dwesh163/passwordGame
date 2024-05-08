d8e5b61ff7892bba4c1f35e775def2216934037c
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-08 11:21:51 +0200

    [fix] 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ export default function notFound() {
 	return (
 		<div className="flex flex-col w-full items-center justify-center min-h-screen bg-white dark:bg-[#171719]">
 			<div className="sm:w-full sm:max-w-sm p-8 w-full">
-				<h2 className="text-center mb-1 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">404</h2>
+				<h1 className="text-center mb-1 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">404</h1>
 				<h2 className="text-center sm:text-xl text-base text-gray-900 mb-8 dark:text-white">Page not found</h2>
 			</div>
 		</div>
