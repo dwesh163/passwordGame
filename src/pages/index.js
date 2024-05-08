44adb2b0d08a970ece574a75d9fe5a531a058e9e
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-08 16:27:09 +0200

    [add] description

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Index() {
 		<>
 			<Head>
 				<title>PasswordGame</title>
-				<meta name="description" content="An app to listen music" />
+				<meta name="description" content="Can you find the password ??" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
